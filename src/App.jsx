2275436eb88043d426e0dbf50b45455954ea1344
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-01-24 17:46:13 -0500

    feat: import FullResume and useState to pass data from inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import { GeneralInformation } from "./components/editor/GeneralInformation";
+import { FullResume } from "./components/resume/FullResume";
+import './App.css'
 
 export function App() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+
+    const fullName = firstName + ' ' + lastName
+    const contactInformation = phoneNumber + ' || ' + email
     return (
-        <GeneralInformation />
+        <div className="mainContainer">
+            <GeneralInformation 
+                firstName={firstName}
+                setFirstName={setFirstName}
+                lastName={lastName}
+                setLastName={setLastName}
+                phoneNumber={phoneNumber}
+                setPhoneNumber={setPhoneNumber}
+                email={email}
+                setEmail={setEmail}
+            />
+            <FullResume 
+                fullName={fullName}
+                contactInformation={contactInformation}
+            />
+        </div>
     )
 }
