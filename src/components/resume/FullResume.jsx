b569da77307f0d3bf4f75362eac0f41930ebc8bb
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-01-27 18:25:15 -0500

    chore: import style and education component

--- src/components/resume/FullResume.jsx
+++ src/components/resume/FullResume.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Header } from "./Header";
+import { Education } from "./Education";
+import '../../styles/FullResume.css'
 
 export function FullResume({
     fullName,
     contactInformation
 }) {
     return(
-        <Header 
-            fullName={fullName}
-            contactInformation={contactInformation}
-        />
+        <div className="resume-container">
+            <Header 
+                fullName={fullName}
+                contactInformation={contactInformation}
+            />
+        </div>
     )
 }
