6c9d17e87d755f62c80ad9ddac5395dfb2c6503d
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-01-28 15:33:26 -0500

    feat: add elements for eduction section

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,9 +1,17 @@
 export function Education({
-    
+    schoolName,
+    degreeInformation,
+    graduationInformation
 }) {
     return (
         <div className="education">
-            
+            <h2>Education</h2>
+            <div className="divider"></div>
+            <div className="degree-date-container">
+                <div className="degree">{degreeInformation}</div>
+                <div className="grad-date">{graduationInformation}</div>
+            </div>
+            <div className="school">{schoolName}</div>
         </div>
     )
 }
