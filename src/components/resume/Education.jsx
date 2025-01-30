051631fc008a4a11db07ce30287417e02e090177
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-01-30 17:00:11 -0500

    feat: updated to use the education arrays/obejects created in App.jsx

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,19 +1,22 @@
 import '../../styles/ResumeEducation.css'
 
 export function Education({
-    schoolName,
-    degreeInformation,
-    graduationInformation
+    educationSections
 }) {
     return (
         <div className="education">
             <h2 className='education-section-header'>Education</h2>
             <div className="divider"></div>
-            <div className="degree-date-container">
-                <div className="degree">{degreeInformation}</div>
-                <div className="grad-date">{graduationInformation}</div>
-            </div>
-            <div className="school">{schoolName}</div>
+            {educationSections.map((section, index) => (
+                <div>
+                    <div key={index} className='degree-date-container'>
+                        <div className="degree">{section.degree}, {section.major}</div>
+                        <div className="grad-date">{section.graduationMonth} {section.graduationYear}</div>
+                    </div>
+                    <div className="school">{section.schoolName}</div>
+                    {index < educationSections.length - 1}
+                </div>
+            ))}
         </div>
     )
 }
