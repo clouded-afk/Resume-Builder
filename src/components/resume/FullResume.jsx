4e3878aab85a4e01712160a0a45c65d29f8a6456
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-05 17:32:33 -0500

    feat: import Skills component and setup the skills section

--- src/components/resume/FullResume.jsx
+++ src/components/resume/FullResume.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./Header";
 import { Education } from "./Education";
 import { WorkExperience } from "./WorkExperience";
+import { Skills } from "./Skills";
 import '../../styles/FullResume.css'
 
 export function FullResume({
     fullName,
     contactInformation,
     educationSections,
-    workSections
+    workSections,
+    skillsSections
 }) {
     return(
         <div className="resume-container">
@@ -21,6 +23,15 @@ export function FullResume({
             <WorkExperience 
                 workSections={workSections}
             />
+            <div className="skills-section">
+                <h2 className="skill-section-header">Skills</h2>
+                <div className="divider"></div>
+                <div className="skills-container">
+                    <Skills 
+                        skillsSections={skillsSections}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
