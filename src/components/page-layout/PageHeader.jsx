187f8012fc36334ee2be1898bc4268813462afbd
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-06 15:38:27 -0500

    chore: import PageHeader.css

--- src/components/page-layout/PageHeader.jsx
+++ src/components/page-layout/PageHeader.jsx
@@ -1,7 +1,11 @@
+import { Button } from "../editor/Button"
+import '../../styles/PageHeader.css'
+
 export function PageHeader() {
     return (
-        <div>
+        <div className="header">
             <h1>Build a Resume</h1>
+            <Button buttonName={"Save"}></Button>
         </div>
     )
 }
