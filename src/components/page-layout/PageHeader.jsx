cecbbf47b96af3912263929865a235e0a18f08bf
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-11 15:41:07 -0500

    feat: add function to save the resume when save button is clicked

--- src/components/page-layout/PageHeader.jsx
+++ src/components/page-layout/PageHeader.jsx
@@ -1,11 +1,32 @@
 import { Button } from "../editor/Button"
+import html2pdf from "html2pdf.js"
 import '../../styles/PageHeader.css'
 
 export function PageHeader() {
+
+    const savePDF = () => {
+        const resume = document.querySelector('.resume-container')
+        
+        const originalBoxShadow = resume.style.boxShadow
+        resume.style.boxShadow = 'none'
+
+        const opt = {
+            margin: [0, 0, 0, 0],
+            filename: 'resume.pdf',
+            image: { type: 'jpeg', qualit: 0.98 },
+            html2canvas: { scale: 2, useCORS: true },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait'}
+        }
+
+        html2pdf().set(opt).from(resume).save().then(() => {
+            resume.style.boxShadow = originalBoxShadow
+        })
+
+    }
     return (
         <div className="header">
             <h1>Build a Resume</h1>
-            <Button buttonName={"Save"}></Button>
+            <Button buttonName={"Save"} onClick={savePDF}></Button>
         </div>
     )
 }
